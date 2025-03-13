d76b71528ac39ea71dad1793297cd2e0653fff68
Roxana Radu <roxanaa.raduu@gmail.com>
2025-03-13 14:09:47 -0700

    badge count when removing item

--- pages/shoppingCartOperations.js
+++ pages/shoppingCartOperations.js
@@ -1,14 +1,21 @@
 import commomActions from "../utils/commonActions.js";
 import { expect } from '@playwright/test';
 
-export default class shoppongCartOperations {
+export default class shoppingCartOperations {
     constructor (page){
         this.actions = new commomActions(page);
         this.shoppingCartIcon = page.locator('[data-test="shopping-cart-link"]');
+        this.removeBackPack = page.locator('[data-test="remove-sauce-labs-backpack"]');
         this.yourCartTitle = page.locator('[data-test="title"]');
         this.shoppingCartBadge = page.locator('[data-test="shopping-cart-badge"]');
     }
     async clickOnShoppingCartIcon  () {
+        await this.shoppingCartIcon.click();
+    }
+    async clickOnRemoveBackPack () {
+        await this.removeBackPack.click();
+    }
+    async clickOnCartIcon () {
         await this.shoppingCartIcon.click();
     }
 }
--- tests/shoppingCartOpetations.spec.js
+++ tests/shoppingCartOpetations.spec.js
@@ -20,14 +20,35 @@ test.describe('Login tests', () => {
     await expect(pm.shoppingCartOperations.yourCartTitle).toHaveText('Your Cart');
 
   });
-  test.only('Test adding multiple products to the cart by clicking on the “Add to cart” button, and verify the number badge updates accordingly', async ({page}) => {
-    await page.pause();
+  test('Test adding multiple products to the cart by clicking on the “Add to cart” button, and verify the number badge updates accordingly', async ({page}) => {
     const numberOfProducts = 3 ;
     //add products to cart
     for (let i=1; i<=numberOfProducts;i++)
     {
-    await pm.productListingandSorting.clickOnAddToCart(i);
+    await pm.productListingandSorting.clickOnAddToCart(1);
     }
     await expect(pm.shoppingCartOperations.shoppingCartBadge).toHaveText(`${numberOfProducts}`);
+  })
+
+  test ('removing a product from the cart by clicking on the “Remove” button, and verify the number badge decreases by one and the product is removed from the cart.', async ({page}) => {
+    const numberOfProductsAdded = 3 ;
+    //add products to cart
+    for (let i=1; i<=numberOfProductsAdded;i++)
+    {
+    await pm.productListingandSorting.clickOnAddToCart(1);
+    }
+      await pm.shoppingCartOperations.clickOnRemoveBackPack();
+      await expect(pm.shoppingCartOperations.shoppingCartBadge).toHaveText(`${numberOfProductsAdded-1}`);
+  })
+
+  test.only ('Product details (quantity, title, description, and price) are displayed correctly on the "Your Cart" page', async ({page}) => {
+    //store title, description, price
+  const title = pm.page.locator(`.inventory_item_name >> nth=${1}`).allInnerTexts();
+
+    //click on add to cart backpack
+    await pm.productListingandSorting.clickOnAddToCart(1);
+    //click on cart icon to be redirected to Your cart
+    await pm.shoppingCartOperations.clickOnCartIcon();
+    await expect (pm.page.locator('[data-test="inventory-item-name"]')).toHaveText(`${title}`);
   })
 });
